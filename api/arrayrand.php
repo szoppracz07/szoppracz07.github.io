6c5836386c65ec5a0f334c0c757a242f3571c8f8
szoppracz07 <47004447+szoppracz07@users.noreply.github.com>
2020-03-24 15:01:12 +0100

    Update arrayrand.php

--- api/arrayrand.php
+++ api/arrayrand.php
@@ -1,3 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta property="og:title" content="szoppracz07 API" />
+	<meta property="og:description" content="szoppracz07 API" />
+	<meta property="og:type" content="Website" />
+	<meta property="og:url" content="https://szoppracz07.github.io/api" />
+	<meta property="og:image" content="https://szoppracz07.github.io/bg.jpg" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="favicon.png"
+	<meta charset="utf-8" />
+    <title>szoppracz07</title>
+	<meta name="description" content="szoppracz07 Official Website! Mini YouTuber! :D!" />
+   <meta name="keywords" content="szoppracz07,discord bots" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	
+</head> 
+<body>
+    
 <?php
 
 $arr = array(
@@ -12,3 +31,6 @@ print_r($arr_rand);
 echo "</pre>";
 
 ?>
+
+</body>    
+</html>
